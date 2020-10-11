38e6fb2070c8800ba6e42782a59d2aa63f85c989
David Soff <david@soff.nl>
2020-10-11 22:53:36 +0200

    Initial app setup

---
+++ app/src/main/java/nl/soffware/madlevel7example/HomeFragment.kt
@@ -0,0 +1,6 @@
+package nl.soffware.madlevel7example
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+}
---
+++ app/src/main/java/nl/soffware/madlevel7example/QuizFragment.kt
@@ -0,0 +1,7 @@
+package nl.soffware.madlevel7example
+
+import androidx.fragment.app.Fragment
+
+class QuizFragment : Fragment(){
+
+}
